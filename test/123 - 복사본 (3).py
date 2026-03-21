472d3dbcfeb87da7ef442f70b4c605624ff5f4d9
savvy773 <subya@naver.com>
2026-03-21 19:08:28 +0900

    Add pr test

--- test/123 - 복사본 (3).py
+++ test/123 - 복사본 (3).py
@@ -1,3 +1,9 @@
 print("hello")
 print("world")
+<<<<<<< HEAD
+<<<<<<<< HEAD:test/123 - 복사본 (3).py
 2222
+========
+>>>>>>>> parent of 08cf9c2 ([Home] 2026-03-21 18:11):test/123 - 복사본 (2).py
+=======
+>>>>>>> parent of 08cf9c2 ([Home] 2026-03-21 18:11)
